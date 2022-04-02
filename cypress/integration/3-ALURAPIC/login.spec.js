689dad0501ed24eb97375e7e31a6b68721c7bc79
gualterapple <39972000+gualterapple@users.noreply.github.com>
2022-04-02 08:07:58 +0100

    Intercepador e teste com mocks feito com sucesso

--- cypress/integration/3-ALURAPIC/login.spec.js
+++ cypress/integration/3-ALURAPIC/login.spec.js
@@ -3,6 +3,9 @@ describe('Login de usuários alura pic', () => {
 
     beforeEach(() => {
         cy.visit('/')
+        cy.intercept('POST','https://apialurapic.herokuapp.com/user/login',{
+            statusCode: 400
+        }).as('stubPost');
     })
 
     it('buscar fotos do flavio', () => {      
@@ -19,6 +22,7 @@ describe('Login de usuários alura pic', () => {
 
     it('fazer login de usuario valido', () => {    
         cy.login(Cypress.env('userName'), Cypress.env('password'));
+        cy.wait('@stubPost')
         cy.contains('a', '(Logout)').should('be.visible'); 
     });
 
